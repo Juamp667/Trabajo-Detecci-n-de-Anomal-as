1c7b87a1c20e28eefebc3eb77493662c75fb35ca
Juamp667 <jmpadillagalera@gmail.com>
2025-11-25 10:11:33 +0100

    fc

---
+++ paquetes/OutliersLibrerias.R
@@ -0,0 +1,16 @@
+# Master -> Detección de anomalías
+# Juan Carlos Cubero. Universidad de Granada
+
+library(tidyverse)  
+library(ggplot2)
+library(fitdistrplus)  # Ajuste de una distribución -> denscomp 
+library(ggpubr)    # ggqqplot -para los gráficos QQ-
+library(ggbiplot)  # biplot
+library(outliers)  # Grubbs
+library(MVN)       # mvn: Test de normalidad multivariante  
+library(CerioliOutlierDetection)  #MCD Hardin Rocke
+library(mvoutlier) # corr.plot 
+library(DescTools) # lof
+library(cluster)   # PAM
+library(heplots)
+
